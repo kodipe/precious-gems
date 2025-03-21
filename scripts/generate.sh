6858bb01dc60b96277f90d211353040dacab6bd9
kodipe <konrad.przydzial@gmail.com>
2025-03-21 11:17:40 +0100

    Update generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,17 +12,15 @@ find content -type f -name '*.md' | while read -r file; do
   # Read the content of the current file
   CONTENT=$(cat "$file")
 
+  DOC_TITLE=$(echo "$CONTENT" | grep -m 1 '^# ' | sed 's/^# //')
+
   # Define the output file name (change extension to .html)
   OUTPUT_FILE="${file%.*}.html"
 
   # Use awk to replace {{ content }} properly
-  awk -v content="$CONTENT" '
-    BEGIN {
-      match(content, /^# (.*)$/, title)
-      gsub(/\{\{ docTitle \}\}/, title[0])
-    }
+  awk -v content="$CONTENT" -v title="$DOC_TITLE" '
+    {gsub(/\{\{ docTitle \}\}/, title)}
     {gsub(/\{\{ content \}\}/, content)}
-    { print }
   ' "$TEMPLATE" > "$OUTPUT_FILE"
 
   echo "Processed: $file -> $OUTPUT_FILE"
