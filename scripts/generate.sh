c00e0e399203c19e6bcd20b1a763629f66eb0c73
kodipe <konrad.przydzial@gmail.com>
2025-03-21 12:35:11 +0100

    Update generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -21,6 +21,7 @@ find content -type f -name '*.md' | while read -r file; do
   awk -v content="$CONTENT" -v title="$DOC_TITLE" '
     {gsub(/\{\{ docTitle \}\}/, title)}
     {gsub(/\{\{ content \}\}/, content)}
+    {gsub(/\[([^\]]+)\]\([^\)]+\)/, "<a href=\"\\2\">\\1</a>")}
     {print}
   ' "$TEMPLATE" > "$OUTPUT_FILE"
 
